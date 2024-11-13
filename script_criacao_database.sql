cd7b1c3f36700cd5c181fcb344b1ec733103ee3b
annabmaker <annabmaker@gmail.com>
2024-11-13 20:42:41 -0300

    script_criacao_database.sql

--- script_criacao_database.sql
+++ script_criacao_database.sql
@@ -17,4 +17,25 @@ nome_professor varchar(45),
 sobrenome_professor varchar(45),
 materia varchar(45),
 PRIMARY KEY (ID)
-)
+);
+
+create table turma 
+(codigo_turma int,
+name_turma varchar(255),
+ano int,
+data_inicio int,
+data_termino int,
+PRIMARY KEY (ID)
+);
+
+create table aluno_turma
+(codigo_aluno_turma int, 
+materia varchar (255),
+aluno varchar(255), 
+turma int,
+PRIMARY KEY (ID)
+);
+
+
+
+
