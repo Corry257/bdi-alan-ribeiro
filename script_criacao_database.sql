affd98b0c945cfefae8c2a0a6d54febba4d9cd07
annabmaker <annabmaker@gmail.com>
2024-12-01 12:17:15 -0300

    Trabalho banco de dados

--- script_criacao_database.sql
+++ script_criacao_database.sql
@@ -25,7 +25,7 @@ name_turma varchar(255),
 ano int,
 data_inicio int,
 data_termino int,
-PRIMARY KEY (ID)
+PRIMARY KEY (codigo_turma)
 );
 
 create table aluno_turma
@@ -33,8 +33,26 @@ create table aluno_turma
 materia varchar (255),
 aluno varchar(255), 
 turma int,
-PRIMARY KEY (ID)
+PRIMARY KEY (codigo_aluno_turma)
 );
+
+
+
+
+
+
+
+
+insert into turma (codigo_turma, nome_turma, ano, data_inicio, data_termino) values;
+(101, 'Matemática', 2024, 20240115, 20240610),
+(102, 'Historia', 2024,20240120,20240620);
+
+
+Insert into aluno_turma (codigo_aluno_turma, materia,aluno,turma) values
+(1, 'matemática', 'Lucas Bernado', 101),
+(2, 'matematica', 'Fernanda Souza', 101),
+(3, 'Historia' , 'carlos Oliveira',102),
+(4, 'Historia', 'Beatriz Campos', 102);
 
 
 
